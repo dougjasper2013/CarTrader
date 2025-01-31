4ebf675d0054e159fb8e3dd3e2a3f97871234199
Douglas Jasper <doug.jasper@trios.com>
2025-01-31 08:19:30 -0500

    Added Car Trader Project

---
+++ CarTrader/CarTrader/CarTraderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarTraderApp.swift
+//  CarTrader
+//
+//  Created by Douglas Jasper on 2025-01-31.
+//
+
+import SwiftUI
+
+@main
+struct CarTraderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarTrader/CarTrader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CarTrader
+//
+//  Created by Douglas Jasper on 2025-01-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Car Trader")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
